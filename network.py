5c35c2152977ac082288c56b421bf48e725dac02
Rafael Magano Maio <rafamaio_98@hotmail.com>
2019-10-09 19:42:12 +0100

    primeiro problema de muitos

--- network.py
+++ network.py
@@ -5,21 +5,24 @@ import pickle # used to serialize objects so that the sent data is an object ins
 
 
 class Server:
-    table = {} # key: tableId, value:playersList
-    tableId = 1
+    #table = {} # key: tableId, value:playersList
+    #tableId = 1
     clientDisconnected = False
     # ipv4 tcp socket
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     connections = []
+    playersConnected = {}
     def __init__ (self):
         self.serverSocket.bind(('0.0.0.0', 10001))
         self.serverSocket.listen(1)
         print("Waiting for a connection, Server Started")
         self.numberOfClients = 0
 
+
     def handler(self, client_socket, client_address):
         while True:
-            if not self.numberOfClients % 4:
+            '''
+            if self.numberOfClients < 4:
                 data = client_socket.recv(1024)
                 if not data:
                     print(str(client_address[0]) + ':' + str(client_address[1]), "disconnected")
@@ -35,16 +38,62 @@ class Server:
                     if connection != client_socket:
                         connection.send(data)
 
-                if self.numberOfClients % 4:
-                    self.tableId += 1
-                # self.table[self.tableID].append(player)
-
                 if not data:
                     print(str(client_address[0]) + ':' + str(client_address[1]), "disconnected")
                     self.connections.remove(client_socket)
                     client_socket.close()
                     self.clientDisconnected = True
                     break
+            '''
+
+            ##VERIFICAR SE O USERNAME JÁ FOI ESCOLHIDO
+            validUser=False
+            while(not validUser):
+                validUser=True
+                client_socket.send(bytes("UserName: ", 'utf-8'))
+                username = client_socket.recv(1024).decode()
+                for value in self.playersConnected.values():
+                    if(value[0]==username):
+                        validUser=False
+                        break
+                if(not validUser):
+                    client_socket.send(bytes("This username was already taken", 'utf-8'))
+
+
+
+            client_socket.send(bytes("CC: ", 'utf-8'))
+            cc = client_socket.recv(1024).decode()
+            connection = (client_socket,client_address)
+            self.playersConnected.update({connection : (username,cc)})
+            client_socket.send(bytes("Lobby:\n", 'utf-8'))
+            for value in self.playersConnected.values():
+                if(value[1]!=cc):
+                    client_socket.send(bytes(value[0]+"\n",'utf-8'))
+            client_socket.send(bytes("\nInvite a player (using his username)", 'utf-8'))
+
+
+            #Sending information to other players about the join
+            for k, v in self.playersConnected.items():
+                if(k[0] != client_socket):
+                    k[0].send(bytes(username+" joined the lobby:\n", 'utf-8'))
+                    k[0].send(bytes("Lobby:\n", 'utf-8'))
+                    for value in self.playersConnected.values():
+                        if (value[1] != v[1]):
+                            k[0].send(bytes(value[0] + "\n", 'utf-8'))
+                    k[0].send(bytes("\nInvite a player (using his username)", 'utf-8'))
+
+            invitation = client_socket.recv(1024).decode()
+
+            for k,v in self.playersConnected.items():
+                if v[0] == invitation:
+                    resp = ""
+                    while resp != "y" and resp != "n":
+                        k[0].send(bytes("Do you want to play with "+username+"?[y/n]","utf-8"))
+                        resp = k[0].recv(1024).decode()
+                        if(resp == "y"):
+                            client_socket.send(bytes("yay", 'utf-8'))
+                        elif(resp == "n"):
+                            client_socket.send(bytes("nay", 'utf-8'))
 
     def run(self):
         while True:
