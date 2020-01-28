48849b98b5c8d6ca6d8a763bd2f52326894f0fed
Inês Justo <ines.zita7@gmail.com>
2020-01-28 11:03:54 +0000

    certs and cipher/decipher ecdh

--- ellipticCurveDiffieHellman.py
+++ ellipticCurveDiffieHellman.py
@@ -3,7 +3,7 @@ import base64
 import hashlib
 import sys
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 import os
@@ -27,29 +27,14 @@ class EllipticCurveDiffieHellman:
         self.yOtherEntity = None
         self.K = None
 
-    def DH(self):
-        self.g = 9
-        self.q = 1001
-        self.a = random.randint(5, 10)
-
-        self.y = (self.g ** self.a) % self.q
-
-        print('g: ', self.g, ' (a shared value), n: ', self.q, ' (a prime number)')
-        print('\nAlice calculates:')
-        print('a (Alice random): ', self.a)
-        print('Alice value (A): ', self.y, ' (g^a) mod p')
-
-        print('\nAlice calculates:')
-
-        keyA = (self.yOtherEntity ** self.a) % self.q
-        print('Key: ', keyA, ' (B^a) mod p')
-        print('Key: ', hashlib.sha256(str(keyA)).hexdigest())
-
     def sign(self, data):
         private_key = ec.generate_private_key(ec.SECP384R1(), default_backend())
         if not data:
             data = b"this is some data I'd like to sign"
         signature = private_key.sign(data, ec.ECDSA(hashes.SHA256()))
+
+    def validateSignature(self, signature, data):
+        return self.exchange_public_key.verify(signature, data, ec.ECDSA(hashes.SHA256()))
 
     def sharedKeyECDHE(self, peer_public_key):
         shared_key = self.exchange_private_key.exchange(ec.ECDH(), peer_public_key)
@@ -61,6 +46,10 @@ class EllipticCurveDiffieHellman:
         # Generate a private key for use in the exchange.
         self.exchange_private_key = ec.generate_private_key(ec.SECP384R1(), default_backend())
         self.exchange_public_key = self.exchange_private_key.public_key()
+        self.serialized_public = self.public_key.public_bytes(encoding=serialization.Encoding.PEM,
+                                                              format=serialization.PublicFormat.SubjectPublicKeyInfo)
+
+        loaded_public_key = serialization.load_pem_public_key(self.serialized_public, backend = default_backend())
 
     def cipherUsingSharedKey(self, shared_key, msg):
         # Setup cipher: AES in CBC mode, w/ a random IV and PKCS #7 padding (similar to PKCS #5)
@@ -72,13 +61,13 @@ class EllipticCurveDiffieHellman:
         maxLenG = algorithms.AES.block_size
         maxLen = maxLenG
         minLen = 0
-        plaintext = bytes()
+        ciphertext = bytes()
         if maxLenG >= len(msg):
             ciphertext = encryptor.update(padder.update(msg) + padder.finalize())
         else:
             finalize = False
-            # Cycle to repeat while there is data left on the input file
-            # Read a chunk of the input file to the plaintext variable
+            # Cycle to repeat while there is data left on the input data
+            # Read a chunk of the input data to the plaintext variable
             # Use for the chunk length a multiple of the AES data block size
             while minLen < len(msg):
                 plaintext = msg[minLen:maxLen]
@@ -94,3 +83,36 @@ class EllipticCurveDiffieHellman:
                     ciphertext = encryptor.update(padder.update(plaintext))
 
         return ciphertext
+
+    def decipherUsingSharedKey(self, shared_key, msg):
+        # Setup cipher: AES in CBC mode, w/ a random IV and PKCS #7 padding (similar to PKCS #5)
+        iv = os.urandom(algorithms.AES.block_size // 8)
+        cipher = Cipher(algorithms.AES(shared_key), modes.CBC(iv), default_backend())
+        decryptor = cipher.decryptor()
+        padder = padding.PKCS7(algorithms.AES.block_size).padder()
+
+        maxLenG = algorithms.AES.block_size
+        maxLen = maxLenG
+        minLen = 0
+        plaintext = bytes()
+        if maxLenG >= len(msg):
+            plaintext = decryptor.update(padder.update(msg) + padder.finalize())
+        else:
+            finalize = False
+            # Cycle to repeat while there is data left on the input data
+            # Read a chunk of the input data to the ciphertext variable
+            # Use for the chunk length a multiple of the AES data block size
+            while minLen < len(msg):
+                ciphertext = msg[minLen:maxLen]
+                minLen = maxLen
+                if maxLen + maxLenG > len(msg):
+                    maxLen += maxLenG
+                else:
+                    maxLen = len(msg)
+                    finalize = True
+                if finalize:
+                    plaintext = decryptor.update(padder.update(ciphertext) + padder.finalize())
+                else:
+                    plaintext = decryptor.update(padder.update(ciphertext))
+
+        return plaintext
